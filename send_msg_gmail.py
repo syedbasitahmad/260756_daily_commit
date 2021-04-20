820e5f46a382372a546c5903624e0ffe35114b1b
BasitAhmad <basitahmad4@gmail.com>
2021-04-20 13:46:55 +0530

    send msg using gmail from python

---
+++ send_msg_gmail.py
@@ -0,0 +1,20 @@
+import smtplib as s
+
+#creates session(for gmail port 587)
+a = s.SMTP('smtp.gmail.com', 587)
+
+# start TLS security
+a.starttls()
+
+# gmail and password
+a.login("_______________@gmail.com","_____________")
+
+msg="Hello"
+
+#sending mail
+a.sendmail("_______________@gmail.com","___________8@gmail.com",msg)
+
+print("msg sent")
+
+#session quit
+a.quit()
