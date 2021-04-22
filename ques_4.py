569d6b816ce6609ec78b1ffce7b48b8ce8beb554
BasitAhmad <basitahmad4@gmail.com>
2021-04-23 00:30:48 +0530

    abbreviations-ques-4

---
+++ ques_4.py
@@ -0,0 +1,12 @@
+a=input().strip()
+a=a.lower()
+if (a=="lol"):
+    print("laugh out loud")
+elif(a=="rofl"):
+    print("rolling on the floor laughing")
+elif(a=="lmk"):
+    print("let me know")
+elif(a=="smk"):
+    print("shakinhg my head")
+else:
+    print("seems like u use a lot of abbreviations....!!!!!!!!!!!! ")
