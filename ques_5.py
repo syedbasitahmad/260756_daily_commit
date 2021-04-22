c5e2360c2bb0b80e82f88763776de666fa9655be
BasitAhmad <basitahmad4@gmail.com>
2021-04-23 00:45:06 +0530

    price of pizzas-ques-5

---
+++ ques_5.py
@@ -0,0 +1,15 @@
+error_msg="plz type correct input"
+def take_input():
+    no_of_pizzas=int(input())
+    return no_of_pizzas
+def price(no_of_pizzas):
+    if (no_of_pizzas<=0):
+        return error_msg
+    if (no_of_pizzas%2==0):
+        return no_of_pizzas*120
+    else:
+        return no_of_pizzas*130
+if __name__=="__main__":
+    no_of_pizzas=take_input()
+    ans=price(no_of_pizzas)
+    print(ans)
