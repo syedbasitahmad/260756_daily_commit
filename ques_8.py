a15c3313afcb085a39bddd639d0561483ef22e32
BasitAhmad <basitahmad4@gmail.com>
2021-04-23 01:40:53 +0530

    maximum and minimum element from a set-ques-8

---
+++ ques_8.py
@@ -0,0 +1,14 @@
+# Python code to get the maximum and minimum element from a set
+def MAX(sets):
+    return (max(sets))
+      
+
+sets = set([8, 16, 24, 1, 25, 3, 10, 65, 55])
+print(MAX(sets))
+
+def MIN(sets):
+    return (min(sets))
+      
+
+sets_1 = set([4, 12, 10, 9, 4, 13])
+print(MIN(sets_1))
