2cf18396f97cadac97c0de69224faaebeb15f5b6
BasitAhmad <basitahmad4@gmail.com>
2021-04-25 15:36:03 +0530

    dictionary-day-

---
+++ dictionary.py
@@ -0,0 +1,39 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'python', 2: 'quick', 3: 'learner'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'python', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
+
+# Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+# Adding elements one at a time
+Dict[0] = 'python'
+Dict[2] = 'quick'
+Dict[3] = 1
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+# Adding set of values
+# to a single Key
+Dict['Value_set'] = 2, 3, 4
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+# Updating existing Key's Value
+Dict[2] = 'Welcome'
+print("\nUpdated key value: ")
+print(Dict)
+
+# Adding Nested Key value to Dictionary
+Dict[5] = {'Nested' :{'1' : 'Life', '2' : 'learner'}}
+print("\nAdding a Nested Key: ")
+print(Dict)
